d2660d122ca7a5553502f5d206759cf28b34b1dc
Christopher Migliorini <christophermigliorini@Christophers-MacBook-Pro.local>
2018-11-11 21:48:44 -0600

    Add static todo list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,36 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  
+  // This will not pass the data in the production version of the app.
+  // It is a good practice to build apps with static data first.
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: "Walk the dog."
+        }, {
+          id: 2,
+          text: "Take out the trash."
+        }, {
+          id: 3,
+          text: "Cook the goose."
+        }, {
+          id: 4,
+          text: "Chop the chicken."
+        }
+      ]
+    };
+  },
+
   render: function () {
+    var {todos} = this.state;
+
     return (
       <div>
-        Todoapp.jsx
+        <TodoList todos={todos} />
       </div>
     )
   }
